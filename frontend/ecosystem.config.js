d472024e4fed6d42cb15ea38ae5f3b78005f7380
AKatryukhin <A.Katryukhin@yandex.ru>
2025-11-26 09:13:13 +0300

    feat: edited ecosystem.config.js, edited strucrure

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,30 @@
+require('dotenv').config({ path: '~/.env.deploy' });
+
+const { DEPLOY_USER, DEPLOY_BRANCH, DEPLOY_HOST, DEPLOY_PATH, REPO_URL } = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend-service',
+    script: 'serve',
+    cwd: './frontend',
+    args: ['-s', 'build', '-l', '3000'],
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    autorestart: true,
+    max_restarts: 10,
+    restart_delay: 1000,
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_BRANCH,
+      repo: REPO_URL,
+      path: `${DEPLOY_PATH}-frontend`,
+      'post-deploy': 'cd frontend && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'pre-setup': 'which git || sudo apt-get install git -y && (which serve || npm install -g serve)',
+    },
+  },
+};
